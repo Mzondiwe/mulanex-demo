f82fb3ee7c9273bd3e824d3f77edda883502aaf7
Mzondiwe <168481955+Mzondiwe@users.noreply.github.com>
2025-08-14 21:52:50 +0200

    Update passports.js

--- pages/passports.js
+++ pages/passports.js
@@ -1,2 +1,2 @@
-import { passports } from '@/lib/data'
+import { passports } from '../lib/flags'
 export default function Passports(){return (<div className='grid'>{passports.map(p=>(<div key={p.level} className='card' style={{gridColumn:'span 4'}}><h3>{p.level} Passport</h3><p className='sub'>Requirements: {p.requirements}</p><p className='sub'>Perks: {p.perks}</p><button className='btn'>Start</button></div>))}</div>)}
