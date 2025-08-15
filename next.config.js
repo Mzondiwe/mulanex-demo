4d08fe15d4414593d2b5d9d4dce9f7c059657e85
Mzondiwe <168481955+Mzondiwe@users.noreply.github.com>
2025-08-15 14:43:30 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',              // <-- tells Next to produce static 'out/'
-  images: { unoptimized: true }, // <-- avoids Next/Image optimization server
+  output: 'export' // tells Next to create a static site in /out
 };
 module.exports = nextConfig;
